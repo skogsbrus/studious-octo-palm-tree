5201bce8b588983f3c1789f83e85acf0e07b75c9
Johan Andersson <johan-andersson01@protonmail.com>
2023-02-20 15:56:02 +0100

    Handle negative case

--- rm_unregistered_torrents.py
+++ rm_unregistered_torrents.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from transmission_rpc import Client
 from os import environ
+from pathlib import Path
 
 import argparse
+import json
 
 
 def get_args():
@@ -11,6 +13,7 @@ def get_args():
     parser.add_argument('--port', type=str, required=True)
     parser.add_argument('--username', type=str)
     parser.add_argument('--password', type=str)
+    parser.add_argument('--secrets-file', type=Path)
     return parser.parse_args()
 
 
@@ -25,14 +28,23 @@ def main():
     username = environ.get("TRANSMISSION_RPC_USERNAME")
     password = environ.get("TRANSMISSION_RPC_PASSWORD")
 
+    if args.secrets_file:
+        with open(args.secrets_file, "r") as f:
+            secrets = json.load(f)
+        username = secrets['rpc-username']
+        password = secrets['rpc-password']
+
+
     cli = Client(host=host, username=username, password=password, port=args.port)
 
     torrents = cli.get_torrents()
     unregistered = list(filter(is_unregistered, torrents))
 
-    cli.remove_torrent([t.id for t in unregistered], delete_data=False)
-
-    print(f"Removed {len(unregistered)} unregistered torrents")
+    if unregistered:
+        cli.remove_torrent([t.id for t in unregistered], delete_data=False)
+        print(f"Removed {len(unregistered)} unregistered torrents")
+    else:
+        print(f"No unregistered torrents found")
 
 
 if __name__ == "__main__":
